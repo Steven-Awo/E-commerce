a21771f3057ecfaa83c7e88e651f994aee059d3b
Steven-Awo <rotimiawomodu@gmail.com>
2024-03-10 00:11:16 +0100

    Setting up admin, store, cart

---
+++ README.md
@@ -0,0 +1,2 @@
+username-Garden
+password- root1234
--- store/views.py
+++ store/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 
+from .models import *
+
 # Create your views here.
 
 def cart(request):
-    contxt ={}
-    return render(request, 'store/cart.html')
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+
+    contxt = {'items': items}
+    return render(request, 'store/cart.html', contxt)
 
 
 def store(request):
-    contxt ={}
-    return render(request, 'store/store.html')
+    products = Product.objects.all()
+    contxt = {'products': products}
+    return render(request, 'store/store.html', contxt)
 
 
 def checkout(request):
