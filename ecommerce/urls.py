b22b11755895fbe481abc83d97de7d658f60af58
Steven-Awo <rotimiawomodu@gmail.com>
2024-03-18 19:29:51 +0100

    render

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,13 +22,10 @@ from django.conf import settings
 
 from django.conf.urls.static import static
 
-from django.contrib.staticfiles import staticfiles_urlpatterns
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
